cbb7eb389ba3c111eda7c669de86694d14f3a140
Aiden Townsend <aidenticklesgood@gmail.com>
2025-06-02 16:50:53 -0500

    Make project tab expandable

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import fill from "./app/fill_config.js";
 console.log("script loaded");
 
+
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("DOM fully loaded");
 
@@ -10,4 +11,33 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const project_html = await fetch("./app/project_html.html");
   test_element.innerHTML = await project_html.text();
+
+  document.querySelectorAll('.expandable').forEach(async el => {
+
+    el.addEventListener('click', () => {
+      const group = el.dataset.group;
+      document.querySelectorAll(`.long-desc[data-group="${group}"]`)
+        .forEach(detail => {
+          const isExpanded = detail.classList.contains('expanded');
+
+          if (isExpanded) {
+            detail.style.height = detail.scrollHeight + "px";
+
+            requestAnimationFrame(() => {
+              detail.style.height = "0px";
+              detail.classList.remove("expanded");
+            });
+          } else {
+            detail.style.height = detail.scrollHeight + "px";
+            detail.classList.add("expanded");
+
+            detail.addEventListener("transitioned", function tidy() {
+              detail.style.height = "auto";
+              detail.removeEventListener("transitioned", tidy);
+            })
+          }
+        });
+    });
+  });
 });
+
