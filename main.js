9ca520e9e82b91d48b23acae7eb003d6b856c19a
Aiden Townsend <aidenticklesgood@gmail.com>
2025-06-02 13:29:45 -0500

    Start a template HTML for projects

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import fill from "./app/fill_config.js";
-
 console.log("script loaded");
 
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("DOM fully loaded");
 
   fill();
+
+  const test_element = document.getElementById("test");
+
+  const project_html = await fetch("./app/project_html.html");
+  test_element.innerHTML = await project_html.text();
 });
